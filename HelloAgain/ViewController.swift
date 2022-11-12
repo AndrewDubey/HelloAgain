a58fc8ee465e3e9003b5fffaf40313b06fbaf50d
AndrewDubey <gunter015@gmail.com>
2022-11-12 13:46:34 +0500

    1. Add text appearence

--- HelloAgain/ViewController.swift
+++ HelloAgain/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var helloLabel: UILabel!
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        helloLabel.isHidden = true
+        startButton.layer.cornerRadius = 10
     }
-
-
+    
+    @IBAction func startButtonPressed() {
+        if helloLabel.isHidden {
+            helloLabel.isHidden = false
+            startButton.setTitle("Clear text", for: .normal)
+        } else {
+            helloLabel.isHidden = true
+            startButton.setTitle("START", for: .normal)
+        }
+        
+    }
+    
 }
-
